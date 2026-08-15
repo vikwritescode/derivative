4b68502c9d282b7649623f826ebde4da203ffe13
vik <163123415+vikwritescode@users.noreply.github.com>
2026-08-15 11:57:59 +0100

    Update debate insertion to use all parameters

--- src/service/insert_debate.py
+++ src/service/insert_debate.py
@@ -16,8 +16,8 @@ def insert_debate(debate: DebateCreate, uid: str, db: sqlite3.Connection):
         # check if tournament exists before inserting
         if debate.tournament is None:
             # insert without tournament reference if None
-            cur.execute("INSERT INTO debates (user_id, date, position, points, speaks, infoslide, motion) VALUES (?, ?, ?, ?, ?, ?, ?)",
-                        (uid, debate.date, debate.position, debate.points, debate.speaks, debate.infoslide, debate.motion))
+            cur.execute("INSERT INTO debates (user_id, date, position, points, speaks, infoslide, motion, sp_order, has_reply, reply) VALUES (?, ?, ?, ?, ?, ?, ?)",
+                        (uid, debate.date, debate.position, debate.points, debate.speaks, debate.infoslide, debate.motion, debate.order, debate.has_reply, debate.reply))
         else:
             # check if tournament exists
             cur.execute("SELECT tournament_id from tournaments WHERE user_id = ? AND tournament_id = ?",
@@ -26,8 +26,8 @@ def insert_debate(debate: DebateCreate, uid: str, db: sqlite3.Connection):
                 raise RuntimeError("Invalid Tournament ID")
             
             # add tournament to 
-            cur.execute("INSERT INTO debates (user_id, date, position, points, speaks, infoslide, motion, tournament_id) VALUES (?, ?, ?, ?, ?, ?, ?, ?)",
-                        (uid, debate.date, debate.position, debate.points, debate.speaks, debate.infoslide, debate.motion, debate.tournament))
+            cur.execute("INSERT INTO debates (user_id, date, position, points, speaks, infoslide, motion, tournament_id, sp_order, has_reply, reply) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
+                        (uid, debate.date, debate.position, debate.points, debate.speaks, debate.infoslide, debate.motion, debate.tournament, debate.order, debate.has_reply, debate.reply))
         db.commit()
         return cur.lastrowid
     except sqlite3.DatabaseError as e:
